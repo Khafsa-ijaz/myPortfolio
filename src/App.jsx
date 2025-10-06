3287f37a96d3db7ddf2f058b9cead53226646f3d
Khafsa_Ijaz16 <129588557+Khafsa-ijaz@users.noreply.github.com>
2025-10-06 23:47:25 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import {Home} from "./component/project/Home";
  
 // import { Main } from "./component/porfolio/Main";
-  import { Main } from "./component/practice/main";
+  import { Main } from "./component/practice/Main";
 
  const App=() =>
  {
